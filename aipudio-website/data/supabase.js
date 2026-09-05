ebe7ccf98d70ff0392195dd2a87c3965ce193a05
aipudiohelp <aipudio.help@gmail.com>
2026-09-05 16:07:59 +0300

    Update supabase.js

--- aipudio-website/data/supabase.js
+++ aipudio-website/data/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://eqxjzbcbvktnoratrvnk.supabase.co/rest/v1/'
+const supabaseUrl = 'https://eqxjzbcbvktnoratrvnk.supabase.co'
 const supabaseAnonKey = 'sb_publishable_HU8Ujc6E-ypf3rr37NORWw_YWE5_pyN'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
